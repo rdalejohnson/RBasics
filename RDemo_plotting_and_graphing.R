a9517c1132d13b8f61f7df7e662abc2c237eeb29
Robert D Johnson <loosemouth@gmail.com>
2021-12-20 17:23:28 -0600

    more graphs

--- RDemo_plotting_and_graphing.R
+++ RDemo_plotting_and_graphing.R
@@ -1,16 +1,38 @@
+install.packages("vcd")
+
 # PLOTTING/GRAPHING
 
 dat <- iris
 dat$size <- ifelse(dat$Sepal.Length < median(dat$Sepal.Length), "small", "big")
 
-########################## MOSAIC plot #######################
+################# MOSAIC plot : categorical data #######################
 
 #visualize contingency table of two qualitative/categoricals
 
 #table() creates the contingency table
+
+table(dat$Species, dat$size)
 
 mosaicplot(table(dat$Species, dat$size),
            color = TRUE,
            xlab = "Species", # label for x-axis
            ylab = "Size" # label for y-axis
 )
+
+library(vcd)  #visualizing categorical data
+
+mosaic(~ Species + size, data = dat, 
+       direction= c("v", "h"))
+
+
+############### BAR plot : visualize distribution of a qualitative variable  ##
+
+barplot(table(dat$size))  #frequencies are plotted
+
+barplot(prop.table(table(dat$size))) #proportions/relative frequencies
+  
+library(ggplot2)
+
+ggplot(dat) +
+  aes(x = size) +
+  geom_bar()
