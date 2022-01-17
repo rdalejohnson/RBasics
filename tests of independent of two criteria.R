ec57effd8e482b866d616966422459a9e89015a3
Robert D Johnson <loosemouth@gmail.com>
2022-01-17 04:52:56 -0600

    adding discussion of residuals

--- tests of independent of two criteria.R
+++ tests of independent of two criteria.R
@@ -37,7 +37,39 @@ race.folic.acid <- as.table(race.folic.acid)
 
 race.folic.acid
 
-barplot(race.folic.acid, legend=T, beside=T, main= "Folic Acid Use by Race")
+barplot(race.folic.acid, legend=T, beside=FALSE, main= "Folic Acid Use by Race")
+
 
 test <- chisq.test(race.folic.acid)
 test
+
+test$method
+test$observed
+test$expected
+
+####################### RESIDUALS ############################
+
+#The larger the residual, the greater the contribution of the cell to the magnitude of the
+#resulting chi-square obtained value. 
+
+#https://scholarworks.umass.edu/cgi/viewcontent.cgi?article=1269&context=pare
+#cells with the largest expected values also produce the largest raw residuals. To
+#overcome that redundancy, a standardized or Pearson
+#residual is calculated by dividing the raw residual by the
+#square root of the expected value as an estimate of the
+#raw residual’s standard deviation:
+
+#The unstandardized/raw residual is the simple difference of the
+#observed and expected values. Unstandardized residual = O - E
+
+# RESIDUALS produced by chisq.test are computed as: 
+#(observed - expected) / sqrt(expected).
+test$residuals
+
+# STANDARDIZED RESIDUALS are computed as  (observed - expected) / sqrt(V)
+# where V is the residual cell variance
+test$stdres
+
+#Adjusted standardized residuals are standardized residuals that are adjusted for the
+#row and column totals. The adjusted standardized residual is defined as:
+#Adjusted standardized residual = O - E / SQRT[nA * nB * (1 - nA/N) * (1 - nB/N) / N]
